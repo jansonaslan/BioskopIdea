94ef933cc33c8b3fa53e73168924fdb3a01968d5
Christian Janson <christianjanson99@gmail.com>
2026-07-02 17:25:29 +0700

    update data

--- src/main/java/com/bioskop/helloworld/DashboardController.java
+++ src/main/java/com/bioskop/helloworld/DashboardController.java
@@ -10,6 +10,9 @@ public class DashboardController {
     private ImageView pacificRimImg;
 
     @FXML
+    private ImageView ingloriousImg;
+
+    @FXML
     private ImageView moana1Img;
 
     @FXML
@@ -24,17 +27,23 @@ public class DashboardController {
 
         pacificRimImg.setImage(pacificRim);
 
-        Image moana = new Image(
+        Image moana1 = new Image(
                 getClass().getResource("/Image/moana1.jpeg").toExternalForm()
         );
 
-        moana1Img.setImage(moana);
+        moana1Img.setImage(moana1);
+
+        Image inglorious= new Image(
+                getClass().getResource("/Image/inglorious.jpg").toExternalForm()
+        );
+
+        ingloriousImg.setImage(inglorious);
 
         Image moana2 = new Image(
                 getClass().getResource("/Image/Moana2.jpg").toExternalForm()
         );
 
-        moana2Img.setImage(moana);
+        moana2Img.setImage(moana2);
 
     }
 }
